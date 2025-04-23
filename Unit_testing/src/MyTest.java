d6236ec198229faaf76a7b0c904cf9b4dcffcf12
Uzma Tasneem <uzma8567@gmail.com>
2025-04-23 20:11:10 +0530

    unit_testing

---
+++ Unit_testing/src/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+	@DisplayName("Testing")
+	@Test
+	void display() {
+		System.out.println("my program");
+	}
+
+}
